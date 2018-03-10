d58a98cb6d6577dde9c53b53b5fbf50ddf05c6ea
“Brian <brianbolnick@gmail.com>
2018-03-10 00:41:01 -0700

    update api url

--- client/src/tools/api-config.js
+++ client/src/tools/api-config.js
@@ -6,7 +6,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === 'localhost') {
   backendHost = 'http://localhost:5000';
 } else {
-  backendHost = 'https://icpm.herokuapp.com';
+  backendHost = 'https://gilagoat.herokuapp.com';
 }
 
 export const API_URL = `${backendHost}`;
